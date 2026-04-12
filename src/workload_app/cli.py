e43410984aa2fd52aa86075bfb320b6a118a3a24
yuberalberto <yuberalberto@gmail.com>
2026-04-12 16:27:26 -0400

    feat: improve CLI output with formatted workload report and block calculation

--- src/workload_app/cli.py
+++ src/workload_app/cli.py
@@ -21,7 +21,7 @@ def main():
         try:
             eps_height, holeformers = extract_puck_data(pdf_file)
 
-            total_eps += eps_height
+            total_eps += eps_height + 20  # Add 20mm as a cutting allowance for the puck
             total_pieces += 1
             for holeformer in holeformers:
                 holeformer_counts[holeformer] = holeformer_counts.get(holeformer, 0) + 1
@@ -36,10 +36,35 @@ def main():
             total_eps += sheets * chart["length"]
 
     score = get_score(total_eps)
+    required_blocks = ceil(total_eps / 2400)  # Where each block is 2400mm length
+
+    holeformers_list = ""
+    for holeformer, count in holeformer_counts.items():
+        holeformers_list += (
+            f"{holeformer[0]} {holeformer[1]} : {count}\n               "
+        )
+
+    # print(
+    #     f"Total EPS height: {total_eps}, Total pieces: {total_pieces}, Score: {score}, Required blocks: {required_blocks}"
+    # )
+    # print(holeformer_counts)
+
     print(
-        f"Total EPS height: {total_eps}, Total pieces: {total_pieces}, Score: {score}"
+        f"""
+            =============================
+                WORKLOAD REPORT
+            =============================
+            Orders processed : {total_pieces}
+            Effort           : {score}
+            -----------------------------
+            Total EPS processed: {total_eps} mm
+            Blocks needed    : {required_blocks}
+            -----------------------------
+            Holeformers counted:
+               {holeformers_list}
+            =============================
+        """
     )
-    print(holeformer_counts)
 
 
 if __name__ == "__main__":
